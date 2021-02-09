39f11558d2d522fab82fe7990a82b52e8f62b275
joseph.L <josephdlee08@gmail.com>
2021-02-09 10:42:12 -0500

    Added a downloader

--- YouTubeGUI.py
+++ YouTubeGUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 from youtubemodule import Youtube
 import requests
 import re
-
+import os
 
 
 
@@ -14,16 +14,23 @@ searchQuery.pack()
 
 def queried():
     yt.getFirst(searchQuery.get())
-    
-sendquery = Button(YTsearch, text='Search!', command=queried)
-canvas = Canvas(YTsearch, width=1000, height=500)
 
-canvas.create_text(300, 50, text='The program opens a tab with the first result of a YouTube query', font=('Times', 11))
-canvas.create_text(256, 70, text='Type something in the search box and hit \'Search!\'', font=('Times', 11))
-canvas.create_text(150, 20, text='YouTube Searcher', font=('Courier', 20))
+def write_video_to_file():
+    with open('downloaded.txt', 'w') as f:
+        f.write(str(yt.getFirst(searchQuery.get())))
+        searcher.create_text(500, 10, text='...Done!')
+
+download = Button(YTsearch, text='Download Video', command=write_video_to_file)
+sendquery = Button(YTsearch, text='Search!', command=queried)
+searcher = Canvas(YTsearch, width=1000, height=500)
+
+searcher.create_text(300, 50, text='The program opens a tab with the first result of a YouTube query', font=('Times', 11))
+searcher.create_text(256, 70, text='Type something in the search box and hit \'Search!\'', font=('Times', 11))
+searcher.create_text(150, 20, text='YouTube Searcher', font=('Courier', 20))
 
 youtubelogo = PhotoImage(file='C:\\Users\\leeso\\OneDrive\\Pictures\\source.gif')
-canvas.create_image(500, 300, image=youtubelogo)
+searcher.create_image(500, 300, image=youtubelogo)
 
+download.pack()
 sendquery.pack()
-canvas.pack()
+searcher.pack()
